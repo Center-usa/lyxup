225b55d895b695d9b47d444870abe6a44864c39d
Center-usa <nabilkamalfahmy@gmail.com>
2026-05-28 19:48:53 +0200

    Update server.js

--- server.js
+++ server.js
@@ -9,10 +9,6 @@ const axios = require("axios");
 const app = express();
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
-
 app.get("/ar", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
