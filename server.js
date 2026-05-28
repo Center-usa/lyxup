0c5bc585749d65103184304bc4bfc50303eaa38e
Center-usa <nabilkamalfahmy@gmail.com>
2026-05-28 20:31:01 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,11 +7,10 @@ const rateLimit = require("express-rate-limit");
 const Stripe = require("stripe");
 const axios = require("axios");
 const app = express();
-app.get("/sitemap.xml", (req, res) => {
-  res.setHeader("Content-Type", "application/xml");
-  res.sendFile(path.join(__dirname, "public", "sitemap.xml"));
+app.get('/sitemap.xml', (req, res) => {
+  res.setHeader('Content-Type', 'application/xml');
+  res.sendFile(path.join(__dirname, 'public', 'sitemap.xml'));
 });
-
 app.get("/ar", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
